0329c198c4c54037eef03781ed73d8c2df6ddd4f
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2022-07-02 12:41:57 +0530

    add: category section

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import { Navbar } from "../../components/navbar/navbar";
 import { Card } from "../../components/card/Card";
+import { Category } from "../../components/category/Category";
 
 const Home = () => {
   return (
     <div className="container">
       <Navbar />
+      <Category />
       <Card />
     </div>
   );
