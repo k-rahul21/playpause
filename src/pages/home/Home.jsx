29f36687ad4d028c336dd67a9cb00d9adc3c51c3
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2022-07-04 22:00:28 +0530

    remove: necessary code

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,5 @@
-import { useState, useEffect } from "react";
 import { Card } from "../../components/card/Card";
 import { Category } from "../../components/category/Category";
-import axios from "axios";
 
 const Home = () => {
   return (
