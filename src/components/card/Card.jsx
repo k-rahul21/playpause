217d2a10bfc8f05831f87ff240d46e32779bb971
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2022-07-04 13:26:25 +0530

    add: card & videos

---
+++ src/components/card/Card.jsx
@@ -0,0 +1,37 @@
+import * as BsIcons from "react-icons/bs";
+import * as RiIcons from "react-icons/ri";
+import "./Card.css";
+
+const Card = () => {
+  return (
+    <div className="card-container">
+      <div className="card">
+        <img
+          className="card-img"
+          src="https://i.ytimg.com/vi/YT7tQzmGRLA/0.jpg"
+        />
+        <div className="card-info">
+          <div className="card-title">
+            <h3 className="card-title-header">10 rules that changed my life</h3>
+            <div className="card-cta">
+              <div className="like-btn">
+                <BsIcons.BsFillSuitHeartFill />
+              </div>
+              <div className="watch-later-btn">
+                <RiIcons.RiPlayListAddFill />
+              </div>
+            </div>
+          </div>
+          <div className="card-description">
+            <h4 className="creator-name">Matt</h4>
+            <span className="verified-icon">
+              <BsIcons.BsFillCheckCircleFill />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export { Card };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
+import { Card } from "../../components/card/Card";
 import { Category } from "../../components/category/Category";
 
 const Home = () => {
   return (
     <div className="container">
       <Category />
+      <Card />
     </div>
   );
 };
