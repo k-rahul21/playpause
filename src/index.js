7a61ed1cec1b27d40a4ca8b7aa608f5b572e1a76
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2022-07-04 22:03:31 +0530

    add: context and server file

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App";
+import { VideoProvider } from "./contexts/video-listing-context";
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <VideoProvider>
+        <App />
+      </VideoProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
