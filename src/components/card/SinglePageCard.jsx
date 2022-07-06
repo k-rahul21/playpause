51a369a6452fe84e1d912f6b4a88b748b8e0119f
Rahul Khandelwal <mrkhandelwal19@gmail.com>
2022-07-06 22:03:14 +0530

    style: to single page card

--- src/components/card/SinglePageCard.jsx
+++ src/components/card/SinglePageCard.jsx
@@ -2,7 +2,7 @@ import * as AiIcons from "react-icons/ai";
 import * as MdIcons from "react-icons/md";
 
 const SinglePageCard = (props) => {
-  const { _id, title, url, description, views, date, creator } = props;
+  const { _id, title, url, description, creator } = props;
   return (
     <div className="video-container">
       <iframe
@@ -16,24 +16,37 @@ const SinglePageCard = (props) => {
       ></iframe>
       <div className="video-footer">
         <div className="footer-title">
-          <h2>{title}</h2>
-          <h5>{creator}</h5>
+          <h2 className="video-title">{title}</h2>
+          <h5 className="video-creator-name">{creator} </h5>
         </div>
-        <div className="footer-btn-list">
-          <div className="like-btn">
-            <AiIcons.AiOutlineLike /> <span>Like</span>
-          </div>
-          <div className="watch-later-btn">
-            <MdIcons.MdOutlineWatchLater /> <span>Watch Later</span>
-          </div>
-          <div className="copy-link-btn">
-            <AiIcons.AiOutlineCopy /> <span>Copy</span>
+        <div className="footer-details">
+          <div className="footer-btn-list">
+            <div className="icon-container like-btn">
+              <div className="like-icon">
+                <AiIcons.AiOutlineLike />
+              </div>
+              <span className="text">Like</span>
+            </div>
+            <div className="icon-container  watch-later-btn">
+              <div className="watch-later-icon">
+                <MdIcons.MdOutlineWatchLater />
+              </div>
+              <span className="text">Watch Later</span>
+            </div>
+            <div className="icon-container  copy-link-btn">
+              <div className=".copy-icon">
+                <AiIcons.AiOutlineCopy />
+              </div>
+              <span className="text">Copy</span>
+            </div>
           </div>
         </div>
+        <div className="divider"></div>
         <div className="footer-description">
-          <h4>Description</h4>
+          <h3 className="sub-heading">Description:</h3>
           <p>{description}</p>
         </div>
+        <div className="divider"></div>
       </div>
     </div>
   );
